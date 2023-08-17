bbf2aed5b52dabea0b1e84699403d89a056c3e88
Joshua.Vazquez@igrtec.com <Joshua.Vazquez@igrtec.com>
2023-08-17 15:21:46 -0600

    ultima update fi

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default
 {
   "expo": {
     "name": "DustBuster",
-    "slug": "login",
+    "slug": "dustbusters",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/images/logoapp.png",
@@ -24,20 +24,29 @@ export default
     "android": {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+        "backgroundColor": "#ffffff",
+       
       }
     },
     "web": {
       "favicon": "./assets/favicon.png"
     },
+    android: {
+      "package": "com.uteq.dustbusters",// Agrega aquí el identificador del paquete
+      versionCode: 1,
+    },
     extra: {
+   
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
-      baseUrl: process.env.BASE_URL
+      baseUrl: process.env.BASE_URL,
+      "eas":{
+        "projectId": "c705e07d-b0a6-4ef7-a966-7f8227a80621"
+      },
     }
   }
 }
